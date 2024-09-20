3acbed39d57638035e5d890e86f3524a84469066
AlejandroZBarajas <falejandrozb@gmail.com>
2024-09-19 20:14:04 -0600

    added: bikesArray

---
+++ src/assets/data/bikesArray.ts
@@ -0,0 +1,127 @@
+const bicicletas = {
+    montaña: {
+      crossCountry: [
+        {
+          img: "",
+          title: "",
+          price: "",
+          transmission: ""
+        },
+        {
+          img: "",
+          title: "",
+          price: "",
+          transmission: ""
+        },
+        {
+          img: "",
+          title: "",
+          price: "",
+          transmission: ""
+        }
+      ],
+      enduro: [
+        {
+          img: "",
+          title: "",
+          price: "",
+          transmission: ""
+        },
+        {
+          img: "",
+          title: "",
+          price: "",
+          transmission: ""
+        },
+        {
+          img: "",
+          title: "",
+          price: "",
+          transmission: ""
+        }
+      ],
+      downhill: [
+        {
+          img: "",
+          title: "",
+          price: "",
+          transmission: ""
+        },
+        {
+          img: "",
+          title: "",
+          price: "",
+          transmission: ""
+        },
+        {
+          img: "",
+          title: "",
+          price: "",
+          transmission: ""
+        }
+      ]
+    },
+    carretera: {
+      climbing: [
+        {
+          img: "",
+          title: "",
+          price: "",
+          transmission: ""
+        },
+        {
+          img: "",
+          title: "",
+          price: "",
+          transmission: ""
+        },
+        {
+          img: "",
+          title: "",
+          price: "",
+          transmission: ""
+        }
+      ],
+      aero: [
+        {
+          img: "",
+          title: "",
+          price: "",
+          transmission: ""
+        },
+        {
+          img: "",
+          title: "",
+          price: "",
+          transmission: ""
+        },
+        {
+          img: "",
+          title: "",
+          price: "",
+          transmission: ""
+        }
+      ],
+      endurance: [
+        {
+          img: "",
+          title: "",
+          price: "",
+          transmission: ""
+        },
+        {
+          img: "",
+          title: "",
+          price: "",
+          transmission: ""
+        },
+        {
+          img: "",
+          title: "",
+          price: "",
+          transmission: ""
+        }
+      ]
+    }
+  };
+  
